c187a9888df9d7ed84c57ec4f115ab36f78417e6
Jin22 <joon7092@snu.ac.kr>
2023-06-07 00:02:18 +0900

    simple test

--- crawler.py
+++ crawler.py
@@ -1,6 +1,23 @@
+import csv
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
 # Goal
 
-def getMostReplayed(video_id):
+# def getMostReplayed(video_id):
 
+#     most_replayed = {}
+#     return most_replayed
+
+
+def getMostReplayed(video_id):
     most_replayed = {}
     return most_replayed
+
+if __name__ == "__main__":
+    df = pd.read_csv('filtered_with_headers.csv')
+    youtube_id_list = df['youtube_id'].tolist()
+    id = youtube_id_list[0]
+    print(id)
+    getMostReplayed(id)
